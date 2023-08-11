06955e6bf4546699d591edb3d10b923e12255e1e
MaxiLopez97 <142050147+MaxiLopez97@users.noreply.github.com>
2023-08-11 17:45:12 -0300

    Initial commit

---
+++ src/Bateria.java
@@ -0,0 +1,18 @@
+
+public class Bateria {
+    private String tipoBateria;
+    private int carga;
+
+    public Bateria() {
+        this.carga = 1000;
+    }
+
+    public String getTipoBateria() {
+        return tipoBateria;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+   
+}
